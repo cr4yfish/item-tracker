c6f9dda99586300282c0f9c3d17675164a5ce71e
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-04-02 18:29:50 +0200

    Added toasts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { createTheme, NextUIProvider } from "@nextui-org/react"
 import { ThemeProvider } from "next-themes"
+import { ToastContainer } from "react-toastify"
 
+import 'react-toastify/dist/ReactToastify.css'
 import "../styles/globals.css"
 import "../styles/theme.css"
 
@@ -13,6 +15,11 @@ function MyApp({ Component, pageProps } : { Component : any, pageProps : any }) 
     <ThemeProvider attribute="class" defaultTheme="dark" value={{dark: theme.className}}>
       <NextUIProvider>
         <Component {...pageProps} />
+        <ToastContainer
+          theme="dark"
+          position="top-center"
+          pauseOnHover
+        />
       </NextUIProvider>
     </ThemeProvider>
   )
